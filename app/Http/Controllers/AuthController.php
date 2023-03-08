edab7cea6ca8e71b1badf8f8cd3956379f8f37e2
sajjad <mynameissajjad@gmail.com>
2023-03-08 16:40:31 +0330

    save user registration to database

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,24 +4,41 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
+use \App\Models\User;
 
 class AuthController extends Controller
 {
     //
     public function login() {
-        return request()->validate([
+        $validated_data = request()->validate([
             'email' => ['required' , 'email' , Rule::unique('users' , 'email') ] , 
             'password' => [ 'required' , 'min:3' ] 
         ]);
+
+        $validated_data['password'] = bcrypt($validated_data['password']);
+
+        return $validated_data;
+
         return 'login';
     }
 
     public function register() {
-        return request()->validate([ 
+        $validated_data = request()->validate([ 
             'username' => [  'required' , Rule::unique('users' , 'username')] , 
             'email' => [ 'required' , 'email' , Rule::unique('users' , 'email')] , 
             'password' => [ 'required' , 'min:3']
         ]);
-        return 'register';
+
+        $validated_data['password'] = bcrypt($validated_data['password']);
+
+        User::create([
+            'username' => $validated_data['username'],
+            'email' => $validated_data['email'],
+            'password' => $validated_data['password']
+        ]); 
+
+        return $validated_data;
+
+        //return 'register';
     }
 }
--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -1,9 +1,18 @@
 <section >
 	<form action="/register" method="POST" class="flex flex-col mx-auto my-20  items-center space-y-4">
 		@csrf
-		<input type="username" name="username" placeholder="Username" class="input " >
-		<input type="email" name="email" placeholder="Email" class="input">
-		<input type="password" name="password" placeholder="Password" class="input">
+		<input value="{{ old('username') }}" type="username" name="username" placeholder="Username" class="input" >
+		@error('username')
+			<p class="text-sm text-red-600 w-80">{{ $message }}</p>
+		@enderror
+		<input value="{{ old('email') }}" type="email" name="email" placeholder="Email" class="input">
+		@error('email')
+			<p class="text-sm text-red-600 w-80">{{ $message }}</p>
+		@enderror
+		<input value="{{ old('password') }}" type="password" name="password" placeholder="Password" class="input">
+		@error('password')
+			<p class="text-sm text-red-600 w-80">{{ $message }}</p>
+		@enderror
 		<button type="submit" class="btn">Sign Up</button>
 	</form>
 </section>

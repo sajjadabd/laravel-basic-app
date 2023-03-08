2d1b5af2e69699da57a2455fc6c89fe8fcfab167
sajjad <mynameissajjad@gmail.com>
2023-03-08 14:52:05 +0330

    add hover and active effect to input fields

--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -1,6 +1,6 @@
 <section>
 	<form action="/register" method="POST">
-		<input type="username" placeholder="Username" class="input" >
+		<input type="username" placeholder="Username" class="input " >
 		<input type="email" placeholder="Email" class="input">
 		<input type="password" placeholder="Password" class="input">
 		<button type="submit" class="btn ">Sign Up</button>

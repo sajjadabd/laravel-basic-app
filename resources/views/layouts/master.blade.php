79d0e1ef199d16261daa07e88ca909073bd7426a
sajjad <mynameissajjad@gmail.com>
2023-03-08 20:08:25 +0330

    add page footer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,7 +6,14 @@
 	<title>Document</title>
 	@vite('resources/css/app.css')
 </head>
-<body class="bg-gray-900 text-sky-100">
-	@yield('content')
+<body class="bg-gray-900 text-sky-100 flex flex-col min-h-screen">
+	
+	<main class="flex-1 min-h-full">
+		@yield('content')
+	</main>
+
+	<footer class="text-center my-4">
+		Copyright &copy; {{ date('Y') }}
+	</footer>
 </body>
 </html>

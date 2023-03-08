11f9247c21b3a970480e01f112f9cb6941288823
sajjad <mynameissajjad@gmail.com>
2023-03-08 16:02:54 +0330

    add more validation rules for registeration and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,23 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class AuthController extends Controller
 {
     //
     public function login() {
         return request()->validate([
-            'email' => 'required' , 
-            'password' => 'required'
+            'email' => ['required' , 'email' , Rule::unique('users' , 'email') ] , 
+            'password' => [ 'required' , 'min:3' ] 
         ]);
         return 'login';
     }
 
     public function register() {
         return request()->validate([ 
-            'username' => 'required' , 
-            'email' => 'required' , 
-            'password' => 'required'
+            'username' => [  'required' , Rule::unique('users' , 'username')] , 
+            'email' => [ 'required' , 'email' , Rule::unique('users' , 'email')] , 
+            'password' => [ 'required' , 'min:3']
         ]);
         return 'register';
     }

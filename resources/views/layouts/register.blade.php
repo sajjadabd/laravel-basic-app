dcbeffdca77dd4bbec0c6f464502e5c910a4ceb9
sajjad <mynameissajjad@gmail.com>
2023-03-08 14:50:10 +0330

    add hover and active effect to button

--- resources/views/layouts/register.blade.php
+++ resources/views/layouts/register.blade.php
@@ -3,6 +3,6 @@
 		<input type="username" placeholder="Username" class="input" >
 		<input type="email" placeholder="Email" class="input">
 		<input type="password" placeholder="Password" class="input">
-		<button type="submit" class="btn">Sign Up</button>
+		<button type="submit" class="btn ">Sign Up</button>
 	</form>
 </section>
